e483b03f2ebe97ee39ae654134088a33f61ef1ae
higorasilverio <higorasilverio@outlook.com>
2022-03-03 15:47:05 -0300

    🎨 ✅ Improve beaches controller comunication and tests added

--- src/controllers/beaches.ts
+++ src/controllers/beaches.ts
@@ -1,10 +1,22 @@
+import mongoose from 'mongoose';
 import { Controller, Post } from '@overnightjs/core';
+import { Beach } from '@src/models/beach';
 import { Request, Response } from 'express';
 
 @Controller('beaches')
 export class BeachesController {
   @Post('')
   public async create(req: Request, res: Response): Promise<void> {
-    res.status(201).send({ ...req.body, id: 'fake-id' });
+    try {
+      const beach = new Beach(req.body);
+      const result = await beach.save();
+      res.status(201).send(result);
+    } catch (err: unknown) {
+      if (err instanceof mongoose.Error.ValidationError) {
+        res.status(422).send({ error: (err as Error).message });
+      } else {
+        res.status(500).send({ error: 'Internal Server Error' });
+      }
+    }
   }
 }
--- test/functional/beaches.test.ts
+++ test/functional/beaches.test.ts
@@ -1,16 +1,39 @@
+import { Beach } from '@src/models/beach';
+import { newBeach, newBeachWithWrongLat } from '../fixtures';
+
 describe('Beaches functional testes', () => {
+  beforeAll(async () => await Beach.deleteMany({}));
+
   describe('When creating a beach', () => {
     it('should create a beach with success', async () => {
-      const newBeach = {
-        lat: -33.792726,
-        lng: 151.189824,
-        name: 'Manly',
-        position: 'E',
-      };
-
       const response = await global.testRequest.post('/beaches').send(newBeach);
       expect(response.status).toBe(201);
+      //object containing matches the keys and values, even if includes other keys such as id
       expect(response.body).toEqual(expect.objectContaining(newBeach));
+    });
+
+    it('should return 422 when there is a validatin error', async () => {
+      const response = await global.testRequest
+        .post('/beaches')
+        .send(newBeachWithWrongLat);
+
+      expect(response.status).toBe(422);
+      expect(response.body).toEqual({
+        error:
+          'Beach validation failed: lat: Cast to Number failed for value "invalid_string" (type string) at path "lat"',
+      });
+    });
+
+    it('should return 500 when there is any error other than validation error', async () => {
+      jest
+        .spyOn(Beach.prototype, 'save')
+        .mockRejectedValueOnce('fail to create beach');
+
+      const response = await global.testRequest.post('/beaches').send(newBeach);
+      expect(response.status).toBe(500);
+      expect(response.body).toEqual({
+        error: 'Internal Server Error',
+      });
     });
   });
 });
